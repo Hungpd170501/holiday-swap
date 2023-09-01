dba6b8cdf4e67058219e77b698b2da62f852d134
Pham Phu Hung <80974691+phuhung9292@users.noreply.github.com>
2023-09-01 13:43:22 +0700

    feat(vnpay): minor fix (#4)
    * add payment vnpay
    
    * update api create payment

--- src/main/java/com/example/holidayswap/config/BankingConfig.java
+++ src/main/java/com/example/holidayswap/config/BankingConfig.java
@@ -18,7 +18,7 @@ public class BankingConfig {
 
     public static String vnp_Version = "2.1.0";
     public static String vnp_Command = "pay";
-    public static String orderType = "other";
+    public static String orderType = "100000";
     public static String vnp_ApiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
 
     public static String md5(String message) {
