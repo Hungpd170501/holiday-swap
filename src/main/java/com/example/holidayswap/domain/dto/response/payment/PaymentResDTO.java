7dc8978a5741f88912a6f62fd027298cca6cf6b2
Pham Phu Hung <80974691+phuhung9292@users.noreply.github.com>
2023-09-01 13:43:22 +0700

    feat(vnpay): minor fix (#4)
    * add payment vnpay
    
    * update api create payment

--- src/main/java/com/example/holidayswap/domain/dto/response/payment/PaymentResDTO.java
+++ src/main/java/com/example/holidayswap/domain/dto/response/payment/PaymentResDTO.java
@@ -3,9 +3,11 @@ package com.example.holidayswap.domain.dto.response.payment;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class PaymentResDTO {
+public class PaymentResDTO implements Serializable {
     private String status;
     private String message;
     private String URL;
