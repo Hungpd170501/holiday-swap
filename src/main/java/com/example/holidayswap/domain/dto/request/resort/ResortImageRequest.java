b96c798dadc0080a1aca1b87ae96dc94acfca2c7
ptKien <phamthanhkien2k1@gmail.com>
2023-09-25 09:09:21 +0700

    resort image api

---
+++ src/main/java/com/example/holidayswap/domain/dto/request/resort/ResortImageRequest.java
@@ -0,0 +1,9 @@
+package com.example.holidayswap.domain.dto.request.resort;
+
+import lombok.Data;
+
+@Data
+public class ResortImageRequest {
+    private Long resortId;
+    private String link;
+}
