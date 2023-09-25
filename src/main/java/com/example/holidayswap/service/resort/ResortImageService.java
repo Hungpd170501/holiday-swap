59a58f04625acec503893d1d70a7a84d8f78366c
Kiên Phạm <101553209+kienpham2k1@users.noreply.github.com>
2023-09-25 10:59:52 +0700

    resort image api (#24)

---
+++ src/main/java/com/example/holidayswap/service/resort/ResortImageService.java
@@ -0,0 +1,19 @@
+package com.example.holidayswap.service.resort;
+
+import com.example.holidayswap.domain.dto.response.resort.ResortImageResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface ResortImageService {
+
+    List<ResortImageResponse> gets(Long resortId);
+
+    ResortImageResponse get(Long id);
+
+    ResortImageResponse create(Long resortId, MultipartFile multipartFile);
+
+    ResortImageResponse update(Long Id, MultipartFile multipartFile);
+
+    void delete(Long id);
+}
