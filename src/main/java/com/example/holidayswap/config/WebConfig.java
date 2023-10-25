86ba1792c7d46600ee21720e5c6b69b397e9035a
HungPD <hungpd170501@gmail.com>
2023-10-26 01:40:03 +0700

    fix:global datetime formatter

--- src/main/java/com/example/holidayswap/config/WebConfig.java
+++ src/main/java/com/example/holidayswap/config/WebConfig.java
@@ -1,11 +1,30 @@
 package com.example.holidayswap.config;
 
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.format.datetime.DateFormatterRegistrar;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.format.number.NumberFormatAnnotationFormatterFactory;
+import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -22,4 +41,13 @@ public class WebConfig implements WebMvcConfigurer {
         configurer.defaultContentType(MediaType.APPLICATION_JSON);
     }
 
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.serializerByType(Date.class, new DateSerializer(
+                false, new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")));
+        builder.serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(
+                DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")));
+        converters.add(new MappingJackson2HttpMessageConverter(builder.build()));
+    }
 }
