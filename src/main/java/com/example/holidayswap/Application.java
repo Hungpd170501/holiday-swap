ce988f9fe2ff78525494c96752ebcd73c727043d
HungPD <hungpd170501@gmail.com>
2023-11-06 12:40:17 +0700

    chore: db migration

--- src/main/java/com/example/holidayswap/Application.java
+++ src/main/java/com/example/holidayswap/Application.java
@@ -3,19 +3,13 @@ package com.example.holidayswap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import java.io.IOException;
 
 @SpringBootApplication
 @EnableCaching
 @EnableTransactionManagement
 @EnableScheduling
-@Configuration
 public class Application {
 
     public static void main(String[] args) {
