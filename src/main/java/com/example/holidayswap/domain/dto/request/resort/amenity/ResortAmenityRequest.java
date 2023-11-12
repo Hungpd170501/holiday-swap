e1089e3cd02897ee45e7d2cff06e65e1c69ee763
Pham Phu Hung <80974691+phuhung9292@users.noreply.github.com>
2023-11-12 09:26:23 +0700

    add test Service Resort amenity (#70)

--- src/main/java/com/example/holidayswap/domain/dto/request/resort/amenity/ResortAmenityRequest.java
+++ src/main/java/com/example/holidayswap/domain/dto/request/resort/amenity/ResortAmenityRequest.java
@@ -1,8 +1,10 @@
 package com.example.holidayswap.domain.dto.request.resort.amenity;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ResortAmenityRequest {
     private String resortAmenityName;
     private String resortAmenityDescription;
