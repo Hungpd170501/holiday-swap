b90318a3240af35b3ee538374cca516b033e0969
phuhung9292 <phuhungx1810@gmail.com>
2023-11-30 00:21:11 +0700

    update api boooking

--- src/main/java/com/example/holidayswap/domain/entity/payment/ApartmentWallet.java
+++ src/main/java/com/example/holidayswap/domain/entity/payment/ApartmentWallet.java
@@ -1,4 +1,4 @@
-package com.example.holidayswap.domain.entity.payment;
+//package com.example.holidayswap.domain.entity.payment;
 
 import com.example.holidayswap.domain.entity.property.Property;
 import jakarta.persistence.*;
@@ -7,20 +7,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
-@Table(name = "apartment_wallet")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class ApartmentWallet {
-    @EmbeddedId
-    private ApartmentWalletId id;
-    @MapsId("propertyId")
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "property_id", nullable = false)
-    private Property property;
-    @Column(name = "total_point")
-    private Double totalPoint;
-    // TODO: update transaction for apartment wallet (retaler wallet -> apartment wallet -> owner wallet)
-}
+//@Entity
+//@Table(name = "apartment_wallet")
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class ApartmentWallet {
+//    @EmbeddedId
+//    private ApartmentWalletId id;
+//    @MapsId("propertyId")
+//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+//    @JoinColumn(name = "property_id", nullable = false)
+//    private Property property;
+//    @Column(name = "total_point")
+//    private Double totalPoint;
+//    // TODO: update transaction for apartment wallet (retaler wallet -> apartment wallet -> owner wallet)
+//}
--- src/main/java/com/example/holidayswap/domain/entity/payment/ApartmentWalletId.java
+++ src/main/java/com/example/holidayswap/domain/entity/payment/ApartmentWalletId.java
@@ -1,25 +1,25 @@
-package com.example.holidayswap.domain.entity.payment;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.io.Serializable;
-
-@Getter
-@Setter
-@Embeddable
-@AllArgsConstructor
-@NoArgsConstructor
-public class ApartmentWalletId implements Serializable {
-    private static final long serialVersionUID = -4184519122503267383L;
-    @NotNull
-    @Column(name = "property_id", nullable = false)
-    private Long propertyId;
-    @Column(name = "room_id")
-    private String roomId;
-}
+//package com.example.holidayswap.domain.entity.payment;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Embeddable;
+//import jakarta.validation.constraints.NotNull;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//import java.io.Serializable;
+//
+//@Getter
+//@Setter
+//@Embeddable
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class ApartmentWalletId implements Serializable {
+//    private static final long serialVersionUID = -4184519122503267383L;
+//    @NotNull
+//    @Column(name = "property_id", nullable = false)
+//    private Long propertyId;
+//    @Column(name = "room_id")
+//    private String roomId;
+//}
