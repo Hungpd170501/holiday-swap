3f1775852d3f36eebb956e6a4ed944734550b436
ptKien <phamthanhkien2k1@gmail.com>
2023-12-07 01:45:25 +0700

    fix Rating

--- src/main/java/com/example/holidayswap/domain/entity/booking/Booking.java
+++ src/main/java/com/example/holidayswap/domain/entity/booking/Booking.java
@@ -1,6 +1,7 @@
 package com.example.holidayswap.domain.entity.booking;
 
 import com.example.holidayswap.domain.entity.auth.User;
+import com.example.holidayswap.domain.entity.property.rating.Rating;
 import com.example.holidayswap.domain.entity.property.timeFrame.AvailableTime;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -81,4 +82,7 @@ public class Booking {
 
     @Column(name = "status", nullable = false)
     private EnumBookingStatus.BookingStatus status;
+    @OneToOne(mappedBy = "booking")
+    private Rating rating;
+
 }
