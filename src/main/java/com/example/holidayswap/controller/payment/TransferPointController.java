a921d817a545dd9d9a61f6e8be044d84f2f89364
ptKien <phamthanhkien2k1@gmail.com>
2023-12-12 11:42:33 +0700

    add constrain for amount

--- src/main/java/com/example/holidayswap/controller/payment/TransferPointController.java
+++ src/main/java/com/example/holidayswap/controller/payment/TransferPointController.java
@@ -3,6 +3,7 @@ package com.example.holidayswap.controller.payment;
 import com.example.holidayswap.domain.dto.request.payment.TransferRequest;
 import com.example.holidayswap.domain.dto.response.payment.TransferResponse;
 import com.example.holidayswap.domain.entity.payment.EnumPaymentStatus;
+import com.example.holidayswap.domain.exception.DataIntegrityViolationException;
 import com.example.holidayswap.service.payment.ITransferPointService;
 import com.example.holidayswap.service.payment.IWalletService;
 import com.example.holidayswap.utils.Helper;
@@ -28,6 +29,7 @@ public class TransferPointController {
     @PostMapping
     public ResponseEntity<TransferResponse> transferPoint(@RequestBody TransferRequest request) throws InterruptedException {
         TransferResponse result = null;
+        if (request.getAmount() < 1) throw new DataIntegrityViolationException("Amount must be a integer value!.");
         result = transferPointService.transferPoint(request.getFrom(), request.getTo(), request.getAmount());
         return result == null ? ResponseEntity.badRequest().body(new TransferResponse(EnumPaymentStatus.BankCodeError.BALANCE_NOT_ENOUGH, "fail", Helper.getCurrentDate())) : ResponseEntity.ok(result);
     }
