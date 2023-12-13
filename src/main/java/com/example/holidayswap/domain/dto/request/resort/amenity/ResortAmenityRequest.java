8c1e735da7e0d8fc3cac5a8eb64a099187626451
Kiên Phạm <101553209+kienpham2k1@users.noreply.github.com>
2023-12-13 12:27:05 +0700

    fix (#145)

--- src/main/java/com/example/holidayswap/domain/dto/request/resort/amenity/ResortAmenityRequest.java
+++ src/main/java/com/example/holidayswap/domain/dto/request/resort/amenity/ResortAmenityRequest.java
@@ -1,10 +1,8 @@
 package com.example.holidayswap.domain.dto.request.resort.amenity;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class ResortAmenityRequest {
     private String resortAmenityName;
     private String resortAmenityDescription;
