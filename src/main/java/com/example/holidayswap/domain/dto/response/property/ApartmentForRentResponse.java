ba3a5cf1eb23349e22558ef7ac150e92a315fa7c
ptKien <phamthanhkien2k1@gmail.com>
2023-12-25 15:41:00 +0700

    fix

--- src/main/java/com/example/holidayswap/domain/dto/response/property/ApartmentForRentResponse.java
+++ src/main/java/com/example/holidayswap/domain/dto/response/property/ApartmentForRentResponse.java
@@ -2,9 +2,9 @@ package com.example.holidayswap.domain.dto.response.property;
 
 import com.example.holidayswap.domain.dto.response.auth.UserProfileResponse;
 import com.example.holidayswap.domain.dto.response.booking.TimeHasBooked;
+import com.example.holidayswap.domain.dto.response.property.coOwner.CoOwnerResponse;
 import com.example.holidayswap.domain.dto.response.property.timeFrame.AvailableTimeResponse;
 import com.example.holidayswap.domain.dto.response.resort.ResortResponse;
-import com.example.holidayswap.domain.entity.property.coOwner.CoOwner;
 import lombok.*;
 
 import java.util.List;
@@ -16,7 +16,7 @@ import java.util.List;
 @Builder
 public class ApartmentForRentResponse {
     private AvailableTimeResponse availableTime;
-    private CoOwner coOwner;
+    private CoOwnerResponse coOwner;
     private PropertyResponse property;
     private ResortResponse resort;
     private UserProfileResponse user;
