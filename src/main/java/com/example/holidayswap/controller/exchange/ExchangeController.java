8dcc60b09a963a2a54a867879e35bf653c06e535
Hungpd170501 <hungpd170501@gmail.com>
2024-01-18 21:05:28 +0700

    fix: endpoint

--- src/main/java/com/example/holidayswap/controller/exchange/ExchangeController.java
+++ src/main/java/com/example/holidayswap/controller/exchange/ExchangeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/exchange")
+@RequestMapping("/api/v1/exchange")
 @AllArgsConstructor
 public class ExchangeController {
     private final IExchangeService exchangeService;
